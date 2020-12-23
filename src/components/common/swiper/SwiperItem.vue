badb44229d41a6d539732e96a33a349f2ceb158f
linbossgebest <809621397@qq.com>
2020-12-23 18:05:51 +0800

    add swiper&scroll components

---
+++ src/components/common/swiper/SwiperItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="slide">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slide"
+};
+</script>
+
+<style scoped>
+.slide {
+  width: 100%;
+  flex-shrink: 0;
+}
+
+.slide img {
+  width: 100%;
+  height: 150px;
+}
+</style>
+
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,119 @@
 <template>
-  <div>home</div>
+  <div id="home">
+    <span>home</span>
+    <swiper>
+      <swiperItem v-for="(item, index) in arr" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" />
+        </a>
+      </swiperItem>
+    </swiper>
+    <scroll class="content">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+        <li>11</li>
+        <li>12</li>
+        <li>13</li>
+        <li>14</li>
+        <li>15</li>
+        <li>16</li>
+        <li>17</li>
+        <li>18</li>
+        <li>19</li>
+        <li>20</li>
+        <li>21</li>
+        <li>22</li>
+        <li>23</li>
+        <li>24</li>
+        <li>25</li>
+        <li>26</li>
+        <li>27</li>
+        <li>28</li>
+        <li>29</li>
+        <li>30</li>
+        <li>31</li>
+        <li>32</li>
+        <li>33</li>
+        <li>34</li>
+        <li>35</li>
+        <li>36</li>
+        <li>37</li>
+        <li>38</li>
+        <li>39</li>
+        <li>40</li>
+        <li>41</li>
+        <li>42</li>
+        <li>43</li>
+        <li>44</li>
+        <li>45</li>
+        <li>46</li>
+        <li>47</li>
+        <li>48</li>
+        <li>49</li>
+        <li>50</li>
+      </ul>
+    </scroll>
+  </div>
 </template>
 
 <script>
+import Scroll from "components/common/scroll/Scroll";
+import Swiper from "components/common/swiper/Swiper";
+import SwiperItem from "components/common/swiper/SwiperItem";
+
 export default {
-
-}
-
+  name: "Home",
+  components: {
+    Scroll,
+    Swiper,
+    SwiperItem,
+  },
+  data() {
+    return {
+      arr: [
+        {
+          name: "1",
+          link: "#",
+          image: require("assets/img/swiper/1.jpg"),
+        },
+        {
+          name: "2",
+          link: "#",
+          image: require("assets/img/swiper/2.jpg"),
+        },
+        {
+          name: "3",
+          llink: "#",
+          image: require("assets/img/swiper/3.jpg"),
+        },
+        {
+          name: "4",
+          link: "#",
+          image: require("assets/img/swiper/4.jpg"),
+        },
+        {
+          name: "5",
+          link: "#",
+          image: require("assets/img/swiper/5.jpg"),
+        },
+      ],
+    };
+  },
+};
 </script>
 <style scoped>
+.content {
+  height: 200px;
+  background: red;
+  overflow: hidden;
+}
 </style>
